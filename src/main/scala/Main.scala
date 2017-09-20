715a408f6f0877720544c2e16447f652e2ab88c1
Olivier NOUGUIER <olivier.nouguier@hawk-platform.io>
2017-09-20 14:00:46 +0200

    Add union

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,18 @@
 object Main {
 
+  case class Person(name: String)
+  case class Password(value: String)
+
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     println(msg)
   }
 
   def msg = "I was compiled by dotty :)"
+
+  def union(id: Person | Password) = id match {
+    case Person(name) => name
+    case Password(value) => value
+  }
 
 }
