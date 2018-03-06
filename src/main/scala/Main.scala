3c93e0abcd9e367f1b4f9a6840157d298daa3ec8
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2018-03-06 18:38:31 +0100

    :arrow_up:

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,11 +1,16 @@
 object Main {
 
-  case class Person(name: String)
+sealed trait Identifiable(id: String)
+
+  case class Person(name: String) extends Identifiable(name)
   case class Password(value: String)
 
   def main(args: Array[String]): Unit = {
     println("Hello world!")
     println(msg)
+
+
+
   }
 
   def msg = "I was compiled by dotty :)"
