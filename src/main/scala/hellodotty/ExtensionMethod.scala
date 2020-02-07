fd1d79a0398cf47568b08eebc766cfac9c180432
Olivier NOUGUIER <olivier.nouguier@gmail.com>
2020-02-07 18:30:46 +0100

    Add extension set example.

--- src/main/scala/hellodotty/ExtensionMethod.scala
+++ src/main/scala/hellodotty/ExtensionMethod.scala
@@ -11,7 +11,13 @@ object ExtensionMethod extends App {
 
    def (c: ACircle) circumference: Double = c.radius * math.Pi * 2
 
+
+   extension circleExtensionSet on (c: ACircle) {
+      def volume = c.radius * c.radius * c.radius * 4/3 * math.Pi
+   }
+
    println( ACircle(2).circumference )
 
+   println(ACircle(2).volume)
 
 }
